4f235bb1c325e7920937ce6056a850ca81fc1ffe
N0790170 <tavian.taylor2018@my.ntu.ac.uk>
2021-04-23 16:27:10 +0100

    added morgan log

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 dbURI = "mongodb+srv://admin:admin@nodetut.4yiwl.mongodb.net/todo?retryWrites=true&w=majority";
 mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
 .then((result) => app.listen(3000))
-.catch((err) => err)
+.catch((err) => console.error(err))
 
 app.use(morgan('dev'));
+
